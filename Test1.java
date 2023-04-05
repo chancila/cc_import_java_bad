6767a16874fa0c520951fa09ac98852d20a91792
Cristian Hancila <chancila@snapchat.com>
2023-04-05 12:24:19 -0700

    wip

--- Test1.java
+++ Test1.java
@@ -3,8 +3,6 @@ import org.junit.Test;
 public class Test1 {
     @Test
     public void testFoo() {
-        String[] paths = System.getProperty("java.library.path").split(":");
-        
         System.loadLibrary("bad");
     }
 }
